c4a5a12106016c5104f47b70a7e4da3ce24a1fe6
redundante3452 <99729820+redundante3452@users.noreply.github.com>
2022-03-21 23:49:25 -0500

    Add files via upload

---
+++ Juegos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaJuegos
+{
+    internal class Juegos
+    {
+        public string Nombre { get; set; }
+        public string Genero { get; set; }
+        public int Id { get; set; }
+
+        public void Agregar(string NombreJuego, int Id, string GeneroJuego)
+        {
+            this.Nombre = NombreJuego;
+            this.Id = Id;   
+            this.Genero = GeneroJuego;
+        }
+        
+
+        public override string ToString()
+        {
+            return "Nombre:   " + Nombre + "   ID:   " + Id + "genero:   " + Genero;
+        }
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace TiendaJuegos
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Juegos> NuevoJuego = new List<Juegos>();
+            NuevoJuego.Add(new Juegos() { Nombre = "league of legend", Id = 1, Genero = "AVENTURA" });
+            NuevoJuego.Add(new Juegos() { Nombre = "valorant", Id = 2, Genero = "PLATAFORMA" });
+            NuevoJuego.Add(new Juegos() { Nombre = "Call of duty", Id = 3, Genero = "ACCION" });
+            NuevoJuego.Add(new Juegos() { Nombre = "tetris", Id = 4, Genero = "PUZZLE" });
+            bool control = false;
+            while (control == false)
+            {
+                Console.WriteLine("BIENVENIDOS ");
+                Console.WriteLine("\n Seleccione la opcion 1 para mostrar video juego");
+                Console.WriteLine("\n Seleccione la opcion 2 para agregar video juego");
+                Console.WriteLine("\n Seleccione la opcion 3 para eliminar video juego");
+                Console.WriteLine("\n Seleccione la opcion 4 para buscar video juego");
+                Console.WriteLine("\n Seleccione la opcion x para salir");
+                string opcion = Console.ReadLine(); 
+                Console.WriteLine(opcion);
+                if (opcion == "2")
+                {
+                    Console.WriteLine("\n Ingrese el nombre del video juego:");
+                    string NombreJuego = Console.ReadLine();
+                    Console.WriteLine(opcion);
+                    Console.WriteLine("\n Ingrese el id del video juego:   ");
+                    string idJuego = Console.ReadLine();
+                    int idJuegos = int.Parse(idJuego);
+                    Console.WriteLine(opcion);
+                    Console.WriteLine("\n Ingrese el genero del video juego:");
+                    string GeneroJuego = Console.ReadLine();
+                    Console.WriteLine(opcion);
+
+                    NuevoJuego.Add(new Juegos() { Nombre = NombreJuego, Id = idJuegos, Genero = GeneroJuego });
+
+                } else if (opcion == "x")
+                {
+                    control = true;
+                }
+                else if (opcion == "3")
+                {
+                    Console.WriteLine("\n Ingrese el nombre del video que desea eliminar:");
+                    string NombreJuego = Console.ReadLine();
+                    Console.WriteLine(opcion);
+                    Console.WriteLine("\n Ingrese el id del video juego que desea eliminar:   ");
+                    string idJuego = Console.ReadLine();
+                    int idJuegos = int.Parse(idJuego);
+                    Console.WriteLine(opcion);
+                    Console.WriteLine("\n Ingrese el genero del video juego que desea eliminar:");
+                    string GeneroJuego = Console.ReadLine();
+                    Console.WriteLine(opcion);
+                    NuevoJuego.Remove(new Juegos() { Nombre = NombreJuego, Id = idJuegos, Genero = GeneroJuego });
+                }
+                else if (opcion == "1")
+                {
+                    for (int i = 0; i < NuevoJuego.Count; i++)
+                    {
+                        Console.WriteLine(NuevoJuego[i].ToString());
+
+                    }
+                    Console.ReadKey();
+                }
+            
+                else if (opcion == "4") 
+                {
+                    control = true;
+                }
+            }
+            
+
+        }
+    }
+}
